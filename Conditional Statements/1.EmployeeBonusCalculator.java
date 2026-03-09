f09a634b9bfde872362448a107c5075ba966cca2
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-03-09 23:07:22 +0530

    1.EmployeeBonusCalculator.java

--- Conditional Statements/1.EmployeeBonusCalculator.java
+++ Conditional Statements/1.EmployeeBonusCalculator.java
@@ -1,39 +1,53 @@
 import java.util.Scanner;
-public class EmployeeBonusCalculator {
+public class EmployeeBonusCalculator{
     public static void main(String[] args) {
         Scanner p1=new Scanner(System.in);
         int performanceRating=p1.nextInt();
-        int yearsOfService=p1.nextInt();
+        int yearOfService=p1.nextInt();
         String deptType=p1.next();
         double baseSalary=p1.nextDouble();
-        int bonus=0;
-        double amot=0;
-        String status="Not Eligibile";
-        if(performanceRating<=4){
-            status="Eligible";
-            if(performanceRating==5){
-                if(deptType=="Critical"){
-                    bonus=25;
-                }
-                else{
-                    bonus=20;
-                }
+        double percentage=0;
+        if(deptType.equals("Critical")){
+            if(performanceRating==5 && performanceRating>=5){
+                percentage=25;
+            }
+            else if(performanceRating==4 && performanceRating>=10){
+                percentage=22;
             }
             else if(performanceRating==4){
-                if(deptType=="Critical"){
-                    bonus=15;
-                }
-                else{
-                    bonus=10;
-                }
+                percentage=15;
             }
-            amot=(baseSalary*bonus)/100;
+            else if(performanceRating==3){
+                percentage=10;
+            }
         }
-        System.out.print("Performance Rating: "\n+performanceRating);
-        System.out.print("Years of Service: "+yearsOfService);
-        System.out.print("Department: "+deptType);
-        System.out.print("Bonus Percentage: "+bonus+ "%");
-        System.out.print("Bonus Amount: "+amot);
-        System.out.print("Status: "+status);
+        else if(deptType.equals("Non-Critical")){
+            if(performanceRating==5){
+                percentage=18;
+            }
+            else if(performanceRating==4){
+                percentage=12;
+            }
+            else if(performanceRating==3){
+                percentage=8;
+            }
+        }
+        System.out.println(percentage);
+        double bonus=(baseSalary*percentage)/100;
+        System.out.println("Performance Rating: "+performanceRating);  
+        System.out.println("Years of Service: "+yearOfService);
+        System.out.println("Department: "+deptType);
+        System.out.println("Bonus Percentage: "+percentage+"%");
+        System.out.println("Bonus Amount: $"+bonus);
+        if(performanceRating>=3){
+            System.out.print("Eligible");
+        }
+        else {
+            System.out.print("Not Eligible");
+        }
     }
 }
+
+
+
+ 
