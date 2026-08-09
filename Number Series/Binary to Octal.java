84e4dfb5d08e6fb3d2226eb2cdb2b1fc519715b1
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-08-09 20:39:54 +0530

    Binary to Octal.java

---
+++ Number Series/Binary to Octal.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main{
+    public static int binaryTodecimal(int n){
+        int ans=0;
+        int x=1;
+        while(n>0){
+            int rem=n%10;
+            ans=ans+(rem*x);
+            x*=2;
+            n/=10;
+        }
+        return ans;
+    }
+    public static int decimalTooctal(int n){
+        int ans=0;
+        int x=1;
+        while(n>0){
+            int rem=n%8;
+            ans=ans+(rem*x);
+            x*=10;
+            n/=8;
+        }
+        return ans;
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int ans=binaryTodecimal(n);
+	    int binaryTodecimal=decimalTooctal(ans);
+		System.out.println(binaryTodecimal);
+	}
+}
