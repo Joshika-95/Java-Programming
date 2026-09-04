5e92153e1885890d3314007f4469aca2600ae9c2
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-09-04 15:10:57 +0530

    15.Find Duplicate element.java

---
+++ Array/15.Find Duplicate element.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main{
+    static void findduplicate(int arr[], int size){
+        boolean visited[]=new boolean [size];
+        for(int i=0;i<size;i++){
+            if(visited[i]){
+                continue;
+            }
+            int count=1;
+            for(int j=i+1;j<size;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                    visited[j]=true;
+                }
+            }
+            if(count>1){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size=sc.nextInt();
+		int arr[]=new int [size];
+		for(int i=0;i<size;i++){
+		    arr[i]=sc.nextInt();
+		}
+		findduplicate(arr,size);
+	}
+}
