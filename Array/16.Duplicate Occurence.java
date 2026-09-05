fc087f477e4e46490f1eba3f0bc9f447c37ee877
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-09-05 23:13:29 +0530

    16.Duplicate Occurence.java

---
+++ Array/16.Duplicate Occurence.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main{
+    static void findOccurrence(int arr[], int size){
+        boolean visited[]=new boolean[size];
+        for(int i=0;i<size;i++){
+            if(visited[i]){
+                continue;
+            }
+            int count=1;
+            for(int j=i+1;j<size;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                    visited[j]=true;
+                }
+            }
+            System.out.println(arr[i]+"-"+count);
+        }
+    }
+	public static void main(String[] args) {
+	    	Scanner sc = new Scanner(System.in);
+		int size=sc.nextInt();
+		int arr[]=new int[size];
+		for(int i=0;i<size;i++){
+		    arr[i]=sc.nextInt();
+		}
+		findOccurrence(arr,size);
+	}
+}
