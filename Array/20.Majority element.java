e175f24ed3942daf8bc8facd6fb5dbdfc08d39d6
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-09-05 23:19:14 +0530

    20.Majority element.java

---
+++ Array/20.Majority element.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+    static void majorityElement(int arr[], int size){
+        for(int i=0;i<size;i++){
+            int count=0;
+            for(int j=0;j<size;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count>size/2){
+                System .out.print(arr[i]);
+                return;
+            }
+        }
+        System.out.print(-1);
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int size=sc.nextInt();
+		int arr[]=new int[size];
+		for(int i=0;i<size;i++){
+		    arr[i]=sc.nextInt();
+		}
+		majorityElement(arr,size);
+	}
+}
