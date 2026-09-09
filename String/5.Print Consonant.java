951524bd53afe24c8a94c1106c017dbb4a02fb32
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-09-09 16:42:36 +0530

    5.Print Consonant.java

---
+++ String/5.Print Consonant.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    static void consonant(String s){
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='a' && ch<='z' || ch>='A' && ch<='Z'){
+                if(!(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
+                   ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')){
+                       System.out.print(ch);
+                   }
+            }
+        }
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s=sc.nextLine();
+		consonant(s);
+	}
+}
